3f5996f73adc9f526855e6c7a16c9d7ac005021c
Conor Parrish <conor.parrish@icloud.com>
2017-11-10 13:51:59 -0700

    Allow sorting of data and plots

--- spotifyGrabber.py
+++ spotifyGrabber.py
@@ -18,9 +18,11 @@ def add_tracks(tracks):
         trackList.append(track['id'])
 
 trackList = []
-valenceList = []
+featureList = []
 yearList = []
 albumList = []
+
+curFeature = 'valence'
 
 cid = 'd070d273528b42fb944f2a65f846d78b'
 secret = 'ce6c32a38f924906ae4fc3796c716b5e'
@@ -28,27 +30,44 @@ client_credentials_manager = SpotifyClientCredentials(client_id=cid, client_secr
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 sp.trace=False
 
+# Query playlist on Spotify API and store tracks
 # results = sp.user_playlist('playlistgiants', '6KOwiWg5zwrt83nEcx7HyI', fields="tracks")
 results = sp.user_playlist('foreignsquid', '5E7xOR1eFzkQphQWrOAC5F', fields="tracks")
+# results = sp.user_playlist('spotify', '37i9dQZF1DX3PIPIT6lEg5', fields="tracks")
 tracks = results['tracks']
 add_tracks(tracks)
 while tracks['next']:
     tracks = sp.next(tracks)
     add_tracks(tracks)
 
+# Get audio features for tracks
 for item in trackList:
-	valenceRes = sp.audio_features(item)
-	valenceList.append(valenceRes[0]['valence'])
+	featureRes = sp.audio_features(item)
+	featureList.append(featureRes[0][curFeature])
 
+# Get year of each track
 for item in trackList:
 	albumRes = sp.track(item)['album']
 	albumList.append(albumRes['id'])
-
 for item in albumList:
 	yearRes = sp.album(item)
 	fullDate = yearRes['release_date']
 	yearList.append(fullDate[0:4])
 
-# plt.plot(yearList, valenceList)
-# plt.show()
+# Make tuples and sort
+tupleList = []
+for i in range(0, len(featureList)):
+    tupleList.append((yearList[i], featureList[i]))
+tupleList.sort()
+i = 0
+for curTuple in tupleList:
+    yearList[i] = curTuple[0]
+    featureList[i] = curTuple[1]
+    i+=1
+
+# Plot the data
+plt.plot(yearList, featureList)
+plt.xlabel('year')
+plt.ylabel(curFeature)
+plt.show()
 
